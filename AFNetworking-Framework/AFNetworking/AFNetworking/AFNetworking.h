6412f06a4e8aa609f9d3320ea52360cd39dc9b62
Florent Vilmart <florentvilmart@me.com>
2015-05-07 12:01:54 -0400

    Adds Carthage support

---
+++ AFNetworking-Framework/AFNetworking/AFNetworking/AFNetworking.h
@@ -0,0 +1,31 @@
+//
+//  AFNetworking.h
+//  AFNetworking
+//
+//  Created by Florent Vilmart on 2015-05-07.
+//  Copyright (c) 2015 afnetworking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFNetworking.
+FOUNDATION_EXPORT double AFNetworkingVersionNumber;
+
+//! Project version string for AFNetworking.
+FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNetworking/PublicHeader.h>
+
+
+
+#import <AFNetworking/AFURLSessionManager.h>
+#import <AFNetworking/AFURLRequestSerialization.h>
+#import <AFNetworking/AFHTTPRequestOperation.h>
+#import <AFNetworking/AFHTTPSessionManager.h>
+#import <AFNetworking/AFURLConnectionOperation.h>
+#import <AFNetworking/AFNetworkReachabilityManager.h>
+#import <AFNetworking/AFSecurityPolicy.h>
+#import <AFNetworking/AFHTTPRequestOperationManager.h>
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking/AFURLResponseSerialization.h>
+#import <AFNetworking/AFNetworkActivityIndicatorManager.h>
---
+++ AFNetworking-Framework/AFNetworking/UIKit+AFNetworking/UIKit+AFNetworking.h
@@ -0,0 +1,28 @@
+//
+//  UIKit+AFNetworking.h
+//  UIKit+AFNetworking
+//
+//  Created by Florent Vilmart on 2015-05-07.
+//  Copyright (c) 2015 afnetworking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIKit+AFNetworking.
+FOUNDATION_EXPORT double UIKit_AFNetworkingVersionNumber;
+
+//! Project version string for UIKit+AFNetworking.
+FOUNDATION_EXPORT const unsigned char UIKit_AFNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKit_AFNetworking/PublicHeader.h>
+
+
+
+#import <UIKit_AFNetworking/UIKit+AFNetworking.h>
+#import <UIKit_AFNetworking/UIProgressView+AFNetworking.h>
+#import <UIKit_AFNetworking/UIActivityIndicatorView+AFNetworking.h>
+#import <UIKit_AFNetworking/UIButton+AFNetworking.h>
+#import <UIKit_AFNetworking/UIWebView+AFNetworking.h>
+#import <UIKit_AFNetworking/UIAlertView+AFNetworking.h>
+#import <UIKit_AFNetworking/UIImageView+AFNetworking.h>
+#import <UIKit_AFNetworking/UIRefreshControl+AFNetworking.h>
